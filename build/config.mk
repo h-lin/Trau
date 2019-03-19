f5533f55f0d5d45b558f658b24269d362f86abee
Hsin-Hung Lin <linil2j@gmail.com>
2019-03-19 10:36:13 +0800

    Update repository setting

--- build/config.mk
+++ build/config.mk
@@ -1,5 +1,5 @@
-CUSTOM_Z3_LIB_PATH := /home/osboxes/customZ3/lib
-CUSTOM_Z3_INCLUDE_PATH := /home/osboxes/customZ3/include
+CUSTOM_Z3_LIB_PATH := /Users/hlin/repositories/custom-z3/lib
+CUSTOM_Z3_INCLUDE_PATH := /Users/hlin/repositories/custom-z3/include
 ANTLR_RUNTIME_PATH := /usr/local/include/antlr4-runtime
 
 FOPENMP := 
